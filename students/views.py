fdd22b1c485c151e0e48668ceeff0b9ff4a64a5a
vitaman1987 <vitaman1987@gmail.com>
2017-11-12 12:32:42 +0200

    students context template

--- students/views.py
+++ students/views.py
@@ -1,10 +1,28 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
 #Views for students
 def students_list(request):
-    return render(request, 'students/students_list.html')
+    students = (
+        {'id':1,
+        'first_name':u'Андрій',
+        'last_name':u'Корост',
+        'ticket':235,
+        'image':'img/me.jpeg'},
+        {'id':2,
+         'first_name':u'Максим',
+         'last_name': u'Настенко',
+         'ticket': 2123,
+         'image':'img/piv.png'},
+        {'id':3,
+         'first_name': u'Віктор',
+         'last_name': u'Конєв',
+         'ticket': 156,
+         'image':'img/podoba3.jpg'},
+    )
+    return render(request, 'students/students_list.html', {'students':students})
 
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
